667ab54a9504bb90351db81e72567c85e20f6d60
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-03-22 00:33:16 +0000

    fullstack StarWars app done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +36,155 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users', methods=['POST'])
+def post_user():
+    new_user = request.json
+    person = User( 
+        username = new_user['username'], 
+        email = new_user['email'],
+        firstname = new_user['firstname'],
+        lastname = new_user['lastname'],
+        password = new_user['password']
 
-    return jsonify(response_body), 200
+    ) #finish this line with proper email source
+    db.session.add(person)
+    db.session.commit()
+    return jsonify(new_user), 200
+
+@app.route('/favorites', methods=['POST'])
+def post_favorite():
+    new_favorite = request.json
+    person = Favorites( 
+        owner = new_favorite['owner'], 
+        name = new_favorite['name'],
+        entity_type = new_favorite['entity_type'],
+        entity_id = new_favorite['entity_id']
+
+    ) #finish this line with proper email source
+    db.session.add(person)
+    db.session.commit()
+    return jsonify(new_favorite), 200
+
+
+
+
+@app.route('/characters', methods=['POST'])
+def post_character():
+    new_character = request.json
+    person = Characters( 
+        name = new_character['name'],
+        birthyear = new_character['birthyear'],
+        eyecolor = new_character['eyecolor'],
+        films = "various",
+        height = new_character['height'],
+        homeworld = new_character['homeworld'],
+        url = new_character['url']
+    ) #finish this line with proper email source
+    db.session.add(person)
+    db.session.commit()
+    return jsonify(new_character), 200
+
+
+
+
+
+
+@app.route('/planets', methods=['POST'])
+def post_planet():
+    new_planet = request.json
+    planet = Planets( 
+        name = new_planet['name'],
+        habitantSpecies = new_planet['habitantSpecies'],
+        size = new_planet['size'],
+        url = new_planet['url']
+    ) #finish this line with proper email source
+    db.session.add(planet)
+    db.session.commit()
+    return jsonify(new_planet), 200
+        
+
+
+
+@app.route('/users', methods=['GET']) # Finished
+def get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
+
+# CREATE GET ROUTE FOR FAVORITES
+
+@app.route('/favorites', methods=['GET']) # Finished
+def get_favorites():
+    favorites = Favorites.query.all()
+    all_favorites = list(map(lambda x: x.serialize(), favorites))
+    return jsonify(all_favorites), 200
+
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Characters.query.all() # GET from Favorite class
+    all_characters = list(map(lambda x: x.serialize(), characters))
+    return jsonify(all_characters), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets_favorites = Planets.query.all() # GET from Favorite class
+    all_planets = list(map(lambda x: x.serialize(), planets))
+    return jsonify(all_planets), 200
+
+
+@app.route('/users/<id>', methods=['DELETE'])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    test_user = User.query.get(id)
+    if(test_user is None):
+        return f"User with id: {id} was deleted"
+    else:  
+        return f"User was not deleted - possible error"
+
+
+
+
+@app.route('/favorites/<id>', methods=['DELETE'])
+def favorites_delete(id):
+    favorite = Favorites.query.get(id)
+    db.session.delete(favorite)
+    db.session.commit()
+
+    updated_favorites = Favorites.query.all()
+    updated_favorites = list(map(lambda x: x.serialize(), updated_favorites))
+
+    test_favorite = Favorites.query.get(id)
+    if(test_favorite is None):
+        return jsonify(
+            {
+                "msg":f"Favorite with id: {id} was deleted",
+                "updated_list": updated_favorites
+            }
+        )
+    
+    
+    else:
+        return f"Favorite was not deleted - possible error"
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+    
+
+    
+
+
--- src/models.py
+++ src/models.py
@@ -4,12 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    firstname = db.Column(db.String(120), unique=True, nullable=False)
-    lastname = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    firstname = db.Column(db.String(120), unique=True, nullable=True)
+    lastname = db.Column(db.String(120), unique=True, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=True)
+    username = db.Column(db.String(120), unique=True, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -18,73 +17,89 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "username": self.username,
         }
+            # do not serialize the password, its a security breach
+
     
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     habitantSpecies = db.Column(db.String(120), unique=True, nullable=False)
-    size = db.Column(db.String(120), unique=True, nullable=False)
-    
+    size = db.Column(db.Integer, unique=True, nullable=False)
     url = db.Column(db.String(80), unique=False, nullable=False)
-    
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<Planets %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "habitantSpecies": self.habitantSpecies,
+            "size": self.size,
+            "url": self.url
             # do not serialize the password, its a security breach
         }
     
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    birthyear = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birthyear = db.Column(db.String, unique=False, nullable=False)
     eyecolor = db.Column(db.String(80), unique=False, nullable=False)
     films = db.Column(db.String(80), unique=False, nullable=False)
-    height = db.Column(db.String(80), unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
     homeworld = db.Column(db.String(80), unique=False, nullable=False)
-    url = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    url = db.Column(db.String(80), unique=False, nullable=False)    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Characters %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "birthyear": self.birthyear,
+            "eyecolor": self.eyecolor,
+            "films": self.films,
+            "height": self.height,
+            "homeworld": self.homeworld,
+            "url": self.url
             # do not serialize the password, its a security breach
         }
 
-
-#     
-#     
-#    
     
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    characterid = db.Column(db.String(120), unique=True, nullable=False)
-    entitytype = db.Column(db.String(80), unique=False, nullable=False)
-    favorite = db.Column(db.String(80), unique=False, nullable=False)
-    planetid = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    userfavorites = db.Column(db.Integer, db.ForeignKey(User.id))
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    entity_type = db.Column(db.String(80), unique=False, nullable=False)
+    entity_id = db.Column(db.Integer, unique=False, nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Favorite id=%r>' %    (self.id, 
+                                        self.name, 
+                                        self.entity_type, 
+                                        self.entity_id, 
+                                        self.owner) #finish this with all columns
+                                                            
+                                                              
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "entity_type": self.entity_type,
+            "entity_id": self.entity_id,
+            "owner": self.owner
+
+            # Continue these to end
             # do not serialize the password, its a security breach
         }
+    
+
 
 
